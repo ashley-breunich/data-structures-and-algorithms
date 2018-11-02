2493c0fae4e80413647d5865b84a2159eb3760ce
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-01 17:58:00 -0700

    Get tests working

--- linkedList/lib/linked-lists.js
+++ linkedList/lib/linked-lists.js
@@ -106,6 +106,51 @@ class LinkedList {
     return arr[newIndex];
   }
 
+  mergeLists(ll1,ll2) {
+    if (ll1 == null) return ll2;
+    if (ll2 == null) return ll1;
+
+    let ll3 = new LinkedList();
+    let current1 = ll1.head;
+    let current2 = ll2.head;
+    let node1 = new Node(current1.value);
+    console.log('node1', node1);
+    let node2 = new Node(current2.value);
+    console.log('node2', node2);
+
+    while(current1 || current2) {
+      if (current1 != null && current2 != null) {
+        ll3.append(node1);
+        current1 = current1.next;
+        console.log('current1 next', current1);
+        ll3.append(node2);
+        current2 = current2.next;
+        console.log('current2 next', current2);
+      } else if (current1 == null) {
+        ll3.append(node2);
+        current2 = current2.next;
+      } else {
+        ll3.append(node1);
+        current1 = current1.next;
+      }
+    }
+    console.log('ll3 final', ll3);
+  }
+
+  reverse() {
+    let current = this.head;
+    let next = null;
+    let previous = null;
+  
+    while(current) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+    this.head = previous;
+  }
+
   length() {
     let length = 0;
     let current = this.head;
