ffb4fab06b9e3d9de783fd82a2786b8bc5ab9b07
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-11 22:54:08 -0800

    Resubmit ll merge

--- llMerge/__tests__/lib/linked-lists-tests.js
+++ llMerge/__tests__/lib/linked-lists-tests.js
@@ -1,54 +1,31 @@
 'use strict';
 
-let LL = require('../../lib/linked-lists.js');
+let LL = require('../../ll-merge');
+let list = new LL();
+
 
 describe('Linked List Constructor', () => {
   it('constructor() will be null', ()=> {
-    let list = new LL();
     expect(list.head).toBeNull();
   });
 });
 
-describe('Append', () => {
-  it('Append will keep the same head value when new values are appended.', ()=> {
-    let list = new LL();
-    list.append(1);  
-    list.append(2);
-    list.append(3);
-    expect(list.head.value).toBe(1);
-    expect(list.head.next.value).toBe(2);
-    expect(list.head.next.next.value).toBe(3);
+describe('Merged Linked List', () => {
+  it('mergeLists() will merge together two linked listed', ()=> {
+    let ll1 = new LL();
+    ll1.append(1);
+    ll1.append(2);
+    ll1.append(3);
+    let ll2 = new LL();
+    ll2.append(5);
+    ll2.append(6);
+    ll2.append(7);
+    let newList = list.mergeLists(ll1,ll2);
+    expect(newList.head.value).toBe(1);
+    expect(newList.head.next.value).toBe(5);
+    expect(newList.head.next.next.value).toBe(2);
+    expect(newList.head.next.next.next.value).toBe(6);
+    expect(newList.head.next.next.next.next.value).toBe(3);
+    expect(newList.head.next.next.next.next.next.value).toBe(7);
   });
-
-  it('if there is no head, the head will become the node value entered', ()=> {
-    let list = new LL();
-    list.prepend('1');
-    expect(list.head.value).toBe('1');
-  });
-
 });
-
-// describe('Merged Linked List', () => {
-//   it('mergeLists() will merge together two linked listed', ()=> {
-//     let ll1 = new LL();
-//     ll1.append(1);
-//     ll1.append(2);
-//     ll1.append(3);
-//     console.log('ll1',ll1);
-//     let ll2 = new LL();
-//     ll2.append(5);
-//     ll2.append(6);
-//     ll2.append(7);
-//     console.log('ll2', ll2);
-//     let ll3 = new LL();
-//     console.log('ll3', ll3);
-//     ll3.mergeLists(ll1,ll2);
-//     expect(ll3.head.value).toBe(1);
-//     expect(ll3.head.next.value).toBe(5);
-//     expect(ll3.head.next.next.value).toBe(2);
-//     expect(ll3.head.next.next.next.next.value).toBe(6);
-//     expect(ll3.head.next.next.next.next.next.value).toBe(3);
-//     expect(ll3.head.next.next.next.next.next.next.value).toBe(7);
-//   });
-
-// });
