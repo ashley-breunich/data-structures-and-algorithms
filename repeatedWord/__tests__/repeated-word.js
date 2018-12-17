1c0db56928bae276515928653adf74efe09a18d9
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-12-16 20:19:09 -0800

    Fot the repeated word working

--- repeatedWord/__tests__/repeated-word.js
+++ repeatedWord/__tests__/repeated-word.js
@@ -5,8 +5,16 @@ const codeChallenge = require('../repeated-word.js');
 describe('Repeated Word', () => {
 
   it('it will return the first repeated word', () => {
-    let string = 'Happy birthday to you, happy birthday to you';
+    let string = 'This is a test sentence to check for a repeated word';
     let message = codeChallenge.repeatedWord(string);
-    expect(message).toEqual('happy');
+    expect(message).toEqual('a');
   });
+
+
+  it('if there is no repeated word, it will return a message to the user', () => {
+    let string = 'This is a test sentence';
+    let message = codeChallenge.repeatedWord(string);
+    expect(message).toEqual('There are not repeated words.');
+  });
+
 });
